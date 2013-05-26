2e5e9e3bd8753ffb81b2967b68583238668055e5
Shaun McCormick <shaun@modx.com>
2013-05-26 13:41:02 -0500

    add queue to page

---
+++ queue.php
@@ -0,0 +1,14 @@
+<?php
+require_once dirname(__FILE__).'/lib/Stats.php';
+require_once dirname(__FILE__).'/lib/Site.php';
+$site = new Site();
+
+$s = '';
+$rs = array();
+$stats = new Stats();
+$response = $stats->get('queue',array(),false);
+
+$site->render('queue.html',array(
+    'queue' => $response['object'],
+    'players' => $response['object']['players'],
+));
