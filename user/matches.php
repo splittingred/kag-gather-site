ee8d4728062b5712c688c628252b956da4bf911b
Shaun McCormick <shaun@modx.com>
2013-06-11 23:46:38 -0500

    more matches

--- user/matches.php
+++ user/matches.php
@@ -11,49 +11,22 @@ $username = $_REQUEST['u'];
 
 $cache = new Cacher();
 $stats = new Stats();
-$data = $stats->get('user',array('username' => $username));
-if (empty($data) || empty($data['object'])) {
-    header('Location: http://gather.kag2d.nl/'); exit();
-}
-$user = $data['object'];
-$user['created_at'] = strftime('%b %d, %Y',strtotime($user['created_at']));
-$avatar = $cache->get($user['kag_user'].'.avatar');
-if (empty($avatar)) {
-    $a = @file_get_contents('https://api.kag2d.com/player/'.$user['kag_user'].'/avatar');
-    if (!empty($a)) {
-        $avatar = json_decode($a,true);
-        if (!empty($avatar)) {
-            $cache->set('avatar/'.$user['kag_user'].'.avatar',$avatar,3600);
-        }
-    }
-}
-$placeholders = array_merge(array(),$user);
-$placeholders['avatar'] = $avatar;
-
-ksort($user['stats']);
-$s = '';
-$list = array();
-$st = array();
-$ranks = array();
-foreach ($user['stats'] as $k => $stat) {
-    $list[] = array(
-        'name' => T::translate($k),
-        'value' => intval($stat['times']),
-    );
-    $st[str_replace('.','_',$k)] = intval($stat['times']);
-    $ranks[str_replace('.','_',$k)] = intval($stat['rank']);
-}
-$placeholders['stat_list'] = $list;
-$placeholders['stats'] = $st;
-$placeholders['ranks'] = $ranks;
-if (!empty($placeholders['clan_name'])) {
-    $placeholders['clan_url'] = urlencode($placeholders['clan_name']);
+$data = $stats->get('user/match',array(
+    'username' => $username,
+));
+if (empty($data) || empty($data['results'])) {
+    //header('Location: http://gather.kag2d.nl/'); exit();
 }
 
-
-foreach ($placeholders['recent_matches'] as &$m) {
+foreach ($data['results'] as &$m) {
     $m['created_at'] = strftime('%b %d, %Y at %I:%M %p',strtotime($m['created_at']));
 }
+
+$placeholders = array(
+    'matches' => $data['results'],
+    'total' => $data['total'],
+    'all_total' => $data['all_total'],
+);
 
 $site = new Site();
 $site->render('u/matches.html',$placeholders,array(
