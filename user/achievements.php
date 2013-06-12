db584b8c08ea297e15dbf4651faee793bbd7e9e7
Shaun McCormick <shaun@modx.com>
2013-06-11 23:24:59 -0500

    faster loading of user achievements

--- user/achievements.php
+++ user/achievements.php
@@ -11,34 +11,14 @@ $username = $_REQUEST['u'];
 
 $cache = new Cacher();
 $stats = new Stats();
-$data = $stats->get('user',array('username' => $username));
-if (empty($data) || empty($data['object'])) {
+$data = $stats->get('user/achievement',array('username' => $username));
+if (empty($data) || empty($data['results'])) {
     header('Location: http://gather.kag2d.nl/'); exit();
 }
-$user = $data['object'];
-$user['created_at'] = strftime('%b %d, %Y',strtotime($user['created_at']));
-$placeholders = array_merge(array(),$user);
-
-ksort($user['stats']);
-$s = '';
-$list = array();
-$st = array();
-$ranks = array();
-foreach ($user['stats'] as $k => $stat) {
-    $list[] = array(
-        'name' => T::translate($k),
-        'value' => intval($stat['times']),
-    );
-    $st[str_replace('.','_',$k)] = intval($stat['times']);
-    $ranks[str_replace('.','_',$k)] = intval($stat['rank']);
-}
-$placeholders['stat_list'] = $list;
-$placeholders['stats'] = $st;
-$placeholders['ranks'] = $ranks;
-if (!empty($placeholders['clan_name'])) {
-    $placeholders['clan_url'] = urlencode($placeholders['clan_name']);
-}
-
+$placeholders = array(
+    'achievements' => $data['results'],
+    'total' => $data['total'],
+);
 
 $site = new Site();
 $site->render('u/achievements.html',$placeholders,array(
